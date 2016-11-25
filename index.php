8a05b2213d194c2862ab4b05b8c1d3f89ad94f00
André Eriksen <neskire@outlook.com>
2016-11-25 10:50:44 +0100

    Test med echo

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
 echo "<h1>Hello toilet user </h1>";
 echo file_get_contents("tal.txt");
+
+?>
+<p>Copyright - 2016 - <a href="http://bjan-toilet.azurewebsites.net">http://bjan-toilet.azurewebsites.net</a></p>
