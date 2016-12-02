06ec51fc3042d8870925e215c8457a990bfeb4cc
Benjamin Schultz Larsen <benjaco1996+edu@gmail.com>
2016-12-02 11:25:23 +0100

    navne på toiletterne

--- hentStatistik.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Benjamin
- * Date: 30-11-2016
- * Time: 13:20
- */
-
-echo file_get_contents("http://nodservice.cloudapp.net/WebService.svc/statistics/{$_GET['fra']}/{$_GET['til']}");
--- index.php
+++ index.php
@@ -32,8 +32,16 @@ echo $twig->render('index.twig',
         'page' => 'Forside',
         'ToiletArray' => $decodedDataArray,
         'NewsArray' => $data,
-        'toilet1' => true
-
+        'toilet1' => true,
+        "navne"=> [
+            "1" => "2. sal tv.",
+            "2" => "2. sal th.",
+            "3" => "3. sal ude tv.",
+            "4" => "3. sal ude th.",
+            "5" => "3. sal inde tv.",
+            "6" => "3. sal inde midt",
+            "7" => "3. sal inde spasser kummen",
+        ]
     )
 );
 ?>
